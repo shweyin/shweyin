e676c95d88ef42c379a964a3912b59cf4f1c6e4d
Shweyin <shweyin@gmail.com>
2023-08-18 14:57:08 +0900

    updated projects

--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -4,157 +4,148 @@ import Sprite from "../../Assets/sprite.png";
 import { faCaretDown, faCaretUp } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { motion, AnimatePresence } from "framer-motion";
-import { scaleCorrectors } from "framer-motion/types/projection/styles/scale-correction";
 
 const Profile = () => {
-    const charInfo = [
-        { label: "Name", value: "Shweyin Than" },
-        { label: "Age", value: "24" },
-        { label: "Country", value: "Canada" },
-        { label: "Speaks", value: "English, Japanese" },
-        {
-            label: "Hobbies",
-            value: "Games, Anime, Cycling, Guitar",
-        },
-        { label: "Favourite Show", value: "Hunter x Hunter" },
-        { label: "Equipped Weapon", value: "Umbrella" },
-        {
-            label: "Passive Skill",
-            value: "100% calm - Able to stay calm in situations of high duress",
-        },
-    ];
+  const charInfo = [
+    { label: "Name", value: "Shweyin Than" },
+    { label: "Age", value: "24" },
+    { label: "Country", value: "Canada" },
+    { label: "Speaks", value: "English, Japanese" },
+    {
+      label: "Hobbies",
+      value: "Games, Anime, Cycling, Guitar",
+    },
+    { label: "Favourite Show", value: "Hunter x Hunter" },
+    { label: "Equipped Weapon", value: "Umbrella" },
+    {
+      label: "Passive Skill",
+      value: "100% calm - Able to stay calm in situations of high duress",
+    },
+  ];
 
-    const skills = [
-        { title: "Frontend", value: 80, color: "#3a3ae1", id: "frontend" },
-        { title: "Backend", value: 70, color: "#3e8c3e", id: "backend" },
-        { title: "Wordpress", value: 65, color: "#ff2f2f", id: "wordpress" },
-    ];
+  const skills = [
+    { title: "Frontend", value: 80, color: "#3a3ae1", id: "frontend" },
+    { title: "Backend", value: 70, color: "#3e8c3e", id: "backend" },
+    { title: "Wordpress", value: 65, color: "#ff2f2f", id: "wordpress" },
+  ];
 
-    const [skillDowns, setSkillDowns] = useState({
-        frontend: {
-            skills: "React, Javascript, HTML, SCSS, TailwindCSS",
-            show: false,
-        },
-        backend: { skills: "C++, Python, Java, Firebase, SQL", show: false },
-        wordpress: { skills: "Oxygen", show: false },
-    });
+  const [skillDowns, setSkillDowns] = useState({
+    frontend: {
+      skills: "React, Javascript, HTML, SCSS, TailwindCSS",
+      show: false,
+    },
+    backend: { skills: "C++, Python, Java, Firebase, SQL", show: false },
+    wordpress: { skills: "Oxygen", show: false },
+  });
 
-    return (
-        <div className={styles.profilePage}>
-            <div className={styles.panel}>
-                <div className={styles.cardShrink}>
-                    <div className={styles.character}>
-                        <div className={styles.charClass}>
-                            <span className={styles.level}>
-                                Lv. {new Date().getFullYear() - 1998}
-                            </span>
-                            Software Developer
-                        </div>
-                        <img
-                            className={styles.spriteImg}
-                            src={Sprite}
-                            alt="Shweyin Than Character Sprite"
-                        />
-                        <div className={styles.status}>
-                            <div className={styles.hp}>
-                                HP: <span>100 / 100</span>
-                            </div>
-                            <div className={styles.mp}>
-                                MP: <span>100 / 100</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div className={`${styles.charInfo} ${styles.card}`}>
-                    <div className={styles.header}>Stats</div>
-                    <form>
-                        {charInfo.map((item, index) => (
-                            <div
-                                className={styles.line}
-                                key={`${item.label}-${index}`}
-                            >
-                                <label>{item.label}</label>
-                                <input disabled value={item.value} />
-                            </div>
-                        ))}
-                    </form>
-
-                    <div className={`${styles.skills} ${styles.card}`}>
-                        <div className={styles.header}>Skills</div>
-                        <div className={styles.skillBars}>
-                            {skills.map((item, index) => (
-                                <div
-                                    className={styles.skillBarContainer}
-                                    key={`${item.title}-${index}`}
-                                >
-                                    <label className={styles.skillBarTitle}>
-                                        {item.title}
-                                        <button
-                                            onClick={() =>
-                                                setSkillDowns({
-                                                    ...skillDowns,
-                                                    [item.id]: {
-                                                        ...skillDowns[item.id],
-                                                        show: !skillDowns[
-                                                            item.id
-                                                        ].show,
-                                                    },
-                                                })
-                                            }
-                                        >
-                                            {skillDowns[item.id].show ? (
-                                                <FontAwesomeIcon
-                                                    icon={faCaretUp}
-                                                />
-                                            ) : (
-                                                <FontAwesomeIcon
-                                                    icon={faCaretDown}
-                                                />
-                                            )}
-                                        </button>
-                                    </label>
-
-                                    <AnimatePresence exitBeforeEnter>
-                                        {skillDowns[item.id].show && (
-                                            <motion.div
-                                                key={item.id}
-                                                initial={{ scaleY: 0 }}
-                                                animate={{ scaleY: 1 }}
-                                                exit={{ scaleY: 0 }}
-                                                style={{ originY: 0 }}
-                                                transition={{
-                                                    duration: 0.1,
-                                                    type: "tween",
-                                                    ease: "linear",
-                                                }}
-                                            >
-                                                {skillDowns[item.id].skills}
-                                            </motion.div>
-                                        )}
-                                    </AnimatePresence>
-
-                                    <div
-                                        className={styles.skillBarWrapper}
-                                        style={{
-                                            width: `${item.value}%`,
-                                        }}
-                                    >
-                                        <span
-                                            className={styles.skillBar}
-                                            style={{
-                                                backgroundColor: item.color,
-                                            }}
-                                        ></span>
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className={styles.profilePage}>
+      <div className={styles.panel}>
+        <div className={styles.cardShrink}>
+          <div className={styles.character}>
+            <div className={styles.charClass}>
+              <span className={styles.level}>
+                Lv. {new Date().getFullYear() - 1998}
+              </span>
+              Software Developer
             </div>
+            <img
+              className={styles.spriteImg}
+              src={Sprite}
+              alt="Shweyin Than Character Sprite"
+            />
+            <div className={styles.status}>
+              <div className={styles.hp}>
+                HP: <span>100 / 100</span>
+              </div>
+              <div className={styles.mp}>
+                MP: <span>100 / 100</span>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+
+        <div className={`${styles.charInfo} ${styles.card}`}>
+          <div className={styles.header}>Stats</div>
+          <form>
+            {charInfo.map((item, index) => (
+              <div className={styles.line} key={`${item.label}-${index}`}>
+                <label>{item.label}</label>
+                <input disabled value={item.value} />
+              </div>
+            ))}
+          </form>
+
+          <div className={`${styles.skills} ${styles.card}`}>
+            <div className={styles.header}>Skills</div>
+            <div className={styles.skillBars}>
+              {skills.map((item, index) => (
+                <div
+                  className={styles.skillBarContainer}
+                  key={`${item.title}-${index}`}
+                >
+                  <label className={styles.skillBarTitle}>
+                    {item.title}
+                    <button
+                      onClick={() =>
+                        setSkillDowns({
+                          ...skillDowns,
+                          [item.id]: {
+                            ...skillDowns[item.id],
+                            show: !skillDowns[item.id].show,
+                          },
+                        })
+                      }
+                    >
+                      {skillDowns[item.id].show ? (
+                        <FontAwesomeIcon icon={faCaretUp} />
+                      ) : (
+                        <FontAwesomeIcon icon={faCaretDown} />
+                      )}
+                    </button>
+                  </label>
+
+                  <AnimatePresence exitBeforeEnter>
+                    {skillDowns[item.id].show && (
+                      <motion.div
+                        key={item.id}
+                        initial={{ scaleY: 0 }}
+                        animate={{ scaleY: 1 }}
+                        exit={{ scaleY: 0 }}
+                        style={{ originY: 0 }}
+                        transition={{
+                          duration: 0.1,
+                          type: "tween",
+                          ease: "linear",
+                        }}
+                      >
+                        {skillDowns[item.id].skills}
+                      </motion.div>
+                    )}
+                  </AnimatePresence>
+
+                  <div
+                    className={styles.skillBarWrapper}
+                    style={{
+                      width: `${item.value}%`,
+                    }}
+                  >
+                    <span
+                      className={styles.skillBar}
+                      style={{
+                        backgroundColor: item.color,
+                      }}
+                    ></span>
+                  </div>
+                </div>
+              ))}
+            </div>
+            {/* <button className={styles.detailButton}>View Details</button> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
--- src/Components/Projects/Projects.tsx
+++ src/Components/Projects/Projects.tsx
@@ -6,162 +6,167 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 
 type project = {
-    name: string;
-    details: string;
-    startDate?: string;
-    endDate?: string;
-    link: string;
-    images?: string[];
-    element?: JSX.IntrinsicElements[keyof JSX.IntrinsicElements];
+  name: string;
+  details: string;
+  startDate?: string;
+  endDate?: string;
+  link: string;
+  images?: string[];
+  element?: JSX.IntrinsicElements[keyof JSX.IntrinsicElements];
 };
 
 const projects: project[] = [
-    {
-        name: "Sonatico",
-        details:
-            "A startup working on building a platform custom built for music teachers to conduct online music lessons.",
-        link: "/",
-        element: (
-            <iframe
-                className={styles.videos}
-                width="100%"
-                height="100%"
-                src="https://www.youtube.com/embed/8E7d7DaOnYI"
-                title="YouTube video player"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-            ></iframe>
-        ),
-    },
-    {
-        name: "Leadership Loom",
-        details:
-            "A corporate leadership skills assessment tool built with ReactJs, TailwindCSS, and Firebase.",
-        link: "/",
-        images: [
-            require("../../Assets/loom.PNG"),
-            require("../../Assets/loom1.PNG"),
-            require("../../Assets/loom2.PNG"),
-            require("../../Assets/loom3.PNG"),
-            require("../../Assets/loom4.PNG"),
-        ],
-    },
-    {
-        name: "Mamacre Entertainment Corporate Website",
-        details:
-            "A corporate website built using Wordpress and Oxygen Builder.",
-        link: "/",
-        element: (
-            <iframe
-                className={styles.videos}
-                width="100%"
-                height="100%"
-                src="https://www.youtube.com/embed/JAAEYkq8nvI"
-                title="YouTube video player"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-            ></iframe>
-        ),
-    },
-    {
-        name: "SACHAYS",
-        details:
-            "A non-profit organization website built using Wordpress and Oxygen Builder",
-        link: "/",
-        images: [
-            require("../../Assets/sachays.PNG"),
-            require("../../Assets/sachays1.PNG"),
-            require("../../Assets/sachays2.PNG"),
-            require("../../Assets/sachays3.PNG"),
-            require("../../Assets/sachays4.PNG"),
-        ],
-    },
-    {
-        name: "My Portfolio",
-        details: "The website you are looking at right now!",
-        link: "/",
-    },
+  {
+    name: "Sachays",
+    details:
+      "A non-profit organization website built using Wordpress and Oxygen Builder",
+    link: "https://sachays.ca/",
+    images: [
+      require("../../Assets/sachays.webp"),
+      require("../../Assets/sachays1.webp"),
+      require("../../Assets/sachays2.webp"),
+      require("../../Assets/sachays3.webp"),
+      require("../../Assets/sachays4.webp"),
+    ],
+  },
+  {
+    name: "Sonatico",
+    details:
+      "A startup working on building a platform custom built for music teachers to conduct online music lessons.",
+    link: "https://sonatico.com/",
+    element: (
+      <iframe
+        className={styles.videos}
+        width="100%"
+        height="100%"
+        src="https://www.youtube.com/embed/8E7d7DaOnYI"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+    ),
+  },
+  {
+    name: "Mamacre Entertainment Corporate Website",
+    details: "A corporate website built using Wordpress and Oxygen Builder.",
+    link: "https://mamacrecorp.com/",
+    element: (
+      <iframe
+        className={styles.videos}
+        width="100%"
+        height="100%"
+        src="https://www.youtube.com/embed/JAAEYkq8nvI"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+    ),
+  },
+  {
+    name: "Leadership Loom",
+    details:
+      "A corporate leadership skills assessment tool built with ReactJs, TailwindCSS, and Firebase.",
+    link: "https://leadership-loom-demo.vercel.app/",
+    images: [
+      require("../../Assets/leadershiploom.webp"),
+      require("../../Assets/leadershiploom1.webp"),
+      require("../../Assets/leadershiploom2.webp"),
+      require("../../Assets/leadershiploom3.webp"),
+      require("../../Assets/leadershiploom4.webp"),
+    ],
+  },
+  {
+    name: "Mamacre Entertainment Label Site",
+    details: "A website to showcase Mamacre Entertainment's latest albums",
+    link: "https://mamacre-label.vercel.app/",
+    images: [
+      require("../../Assets/mamacreLabel.webp"),
+      require("../../Assets/mamacreLabel2.webp"),
+    ],
+  },
 ];
 
 const Projects = () => {
-    const [activeProject, setActiveProject] = useState<project>(projects[0]);
-    const [showModal, setShowModal] = useState(false);
+  const [activeProject, setActiveProject] = useState<project>(projects[0]);
+  const [showModal, setShowModal] = useState(false);
 
-    const toggleModal = (item: project) => {
-        setActiveProject(item);
-        setShowModal(!showModal);
-    };
+  const toggleModal = (item: project) => {
+    setActiveProject(item);
+    setShowModal(!showModal);
+  };
 
-    return (
-        <div className={styles.projectsPage}>
-            <div className={`${styles.card} ${styles.projectPanel}`}>
-                <h1>Projects</h1>
-                <div className={styles.projectList}>
-                    <h2>Name</h2>
-                    <ul>
-                        {projects.map((item, index) => (
-                            <li
-                                key={`${item.name}-${index}`}
-                                onClick={() => toggleModal(item)}
-                                style={{
-                                    display: "flex",
-                                    alignItems: "center",
-                                    justifyContent: "space-between",
-                                    cursor: "pointer",
-                                    paddingRight: "1em",
-                                }}
-                            >
-                                {item.name}
-                                <ArrowForwardIosIcon />
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            </div>
-
-            {showModal && (
-                <CustomModal handleClose={() => setShowModal(false)}>
-                    <h2>
-                        <a href={activeProject.link}>{activeProject.name}</a>
-                    </h2>
-                    {activeProject.images && (
-                        <Carousel
-                            className={styles.carousel}
-                            autoPlay={false}
-                            navButtonsAlwaysVisible
-                            cycleNavigation
-                            animation="slide"
-                            NextIcon={<ArrowForwardIosIcon />}
-                            PrevIcon={<ArrowBackIosNewIcon />}
-                            indicatorContainerProps={{
-                                style: { margin: "20px" },
-                            }}
-                            navButtonsProps={{
-                                // Change the colors and radius of the actual buttons. THIS STYLES BOTH BUTTONS
-                                style: {
-                                    backgroundColor: "transparent",
-                                },
-                            }}
-                        >
-                            {activeProject.images?.map((item, index) => (
-                                <img
-                                    className={styles.slideImg}
-                                    key={index}
-                                    src={item}
-                                    alt={item}
-                                ></img>
-                            ))}
-                        </Carousel>
-                    )}
-
-                    {activeProject.element}
-                    <p>{activeProject.details}</p>
-                </CustomModal>
-            )}
+  return (
+    <div className={styles.projectsPage}>
+      <div className={`${styles.card} ${styles.projectPanel}`}>
+        <h1>Projects</h1>
+        <div className={styles.projectList}>
+          <h2>Name</h2>
+          <ul>
+            {projects.map((item, index) => (
+              <li
+                key={`${item.name}-${index}`}
+                onClick={() => toggleModal(item)}
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                  cursor: "pointer",
+                  paddingRight: "1em",
+                }}
+              >
+                {item.name}
+                <ArrowForwardIosIcon />
+              </li>
+            ))}
+          </ul>
         </div>
-    );
+      </div>
+
+      {showModal && (
+        <CustomModal handleClose={() => setShowModal(false)}>
+          <h2>
+            <a href={activeProject.link} target="_blank">
+              {activeProject.name}
+            </a>
+          </h2>
+          {activeProject.images && (
+            <Carousel
+              className={styles.carousel}
+              autoPlay={false}
+              navButtonsAlwaysVisible
+              cycleNavigation
+              animation="slide"
+              NextIcon={<ArrowForwardIosIcon />}
+              PrevIcon={<ArrowBackIosNewIcon />}
+              indicatorContainerProps={{
+                style: { margin: "20px" },
+              }}
+              navButtonsProps={{
+                // Change the colors and radius of the actual buttons. THIS STYLES BOTH BUTTONS
+                style: {
+                  backgroundColor: "transparent",
+                },
+              }}
+            >
+              {activeProject.images?.map((item, index) => (
+                <img
+                  className={styles.slideImg}
+                  key={index}
+                  src={item}
+                  alt={item}
+                ></img>
+              ))}
+            </Carousel>
+          )}
+
+          {activeProject.element}
+          <p>{activeProject.details}</p>
+        </CustomModal>
+      )}
+    </div>
+  );
 };
 
 export default Projects;
