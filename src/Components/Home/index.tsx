58f64e4900a5d83767146a35ce11f06e0d9ab141
Shweyin <shweyin@gmail.com>
2023-11-14 14:25:27 +0900

    add animation to enter button

--- src/Components/Home/index.tsx
+++ src/Components/Home/index.tsx
@@ -7,86 +7,84 @@ import KeyboardArrowDown from "@mui/icons-material/KeyboardArrowDown";
 import TextSwap from "../TextSwap/TextSwap";
 
 const Home = () => {
-    const [time, setTime] = useState(new Date().toLocaleTimeString());
-    const [showEnter, setShowEnter] = useState(false);
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
+  const [showEnter, setShowEnter] = useState(false);
 
-    setTimeout(() => setTime(new Date().toLocaleTimeString()), 1000);
+  setTimeout(() => setTime(new Date().toLocaleTimeString()), 1000);
 
-    useEffect(() => {
-        setTimeout(() => setShowEnter(true), 2000);
-    }, []);
+  useEffect(() => {
+    setTimeout(() => setShowEnter(true), 1000);
+  }, []);
 
-    return (
-        <div className={styles.homePage}>
-            <div className={styles.intro}>
-                <span className={styles.brackets}>{"<div>"}</span>
-                <TextSwap
-                    text1="Hi, my name is..."
-                    text2="こんにちは、僕の名前は"
-                    className=""
-                />
-                <div className={styles.name}>
-                    <span>
-                        {"Shweyin ".split("").map((char, index) => (
-                            <span key={`${char}-${index}`} className="char">
-                                {char === " " ? "\u00A0" : char}
-                            </span>
-                        ))}
-                    </span>
-                    <span>
-                        {"Than".split("").map((char, index) => (
-                            <span key={`${char}-${index}`} className="char">
-                                {char === " " ? "\u00A0" : char}
-                            </span>
-                        ))}
-                    </span>
-                </div>
-                <div className={styles.softwareDeveloper}>
-                    <span>
-                        {"I build ".split("").map((char, index) => (
-                            <span key={`${char}-${index}`} className="char">
-                                {char === " " ? "\u00A0" : char}
-                            </span>
-                        ))}
-                    </span>
-                    <span>
-                        {"websites".split("").map((char, index) => (
-                            <span key={`${char}-${index}`} className="char">
-                                {char === " " ? "\u00A0" : char}
-                            </span>
-                        ))}
-                    </span>
-                </div>
-                <span className={styles.brackets}>{"</div>"}</span>
-            </div>
-            <div className={styles.graphic}>
-                <motion.div
-                    className={styles.topLine}
-                    initial={{ y: "-100%" }}
-                    animate={{ y: 0 }}
-                    transition={{ default: { duration: 1.5 } }}
-                ></motion.div>
-                <div className={styles.circle}>
-                    <div>{new Date().toDateString()}</div>
-                    <div>{time}</div>
-                </div>
-                <motion.div
-                    className={styles.bottomLine}
-                    initial={{ y: "100%" }}
-                    animate={{ y: 0 }}
-                    transition={{ default: { duration: 1.5 } }}
-                ></motion.div>
-            </div>
-            {showEnter && (
-                <div className={styles.enter}>
-                    <Link to="/profile" className={styles.link}>
-                        Enter
-                        <KeyboardArrowDown />
-                    </Link>
-                </div>
-            )}
+  return (
+    <div className={styles.homePage}>
+      <div className={styles.intro}>
+        <span className={styles.brackets}>{"<div>"}</span>
+        <TextSwap
+          text1="Hi, my name is..."
+          text2="こんにちは、僕の名前は"
+          className=""
+        />
+        <div className={styles.name}>
+          <span>
+            {"Shweyin ".split("").map((char, index) => (
+              <span key={`${char}-${index}`} className="char">
+                {char === " " ? "\u00A0" : char}
+              </span>
+            ))}
+          </span>
+          <span>
+            {"Than".split("").map((char, index) => (
+              <span key={`${char}-${index}`} className="char">
+                {char === " " ? "\u00A0" : char}
+              </span>
+            ))}
+          </span>
         </div>
-    );
+        <div className={styles.softwareDeveloper}>
+          <span>
+            {"I build ".split("").map((char, index) => (
+              <span key={`${char}-${index}`} className="char">
+                {char === " " ? "\u00A0" : char}
+              </span>
+            ))}
+          </span>
+          <span>
+            {"websites".split("").map((char, index) => (
+              <span key={`${char}-${index}`} className="char">
+                {char === " " ? "\u00A0" : char}
+              </span>
+            ))}
+          </span>
+        </div>
+        <span className={styles.brackets}>{"</div>"}</span>
+      </div>
+      <div className={styles.graphic}>
+        <motion.div
+          className={styles.topLine}
+          initial={{ y: "-100%" }}
+          animate={{ y: 0 }}
+          transition={{ default: { duration: 1.5 } }}
+        ></motion.div>
+        <div className={styles.circle}>
+          <div>{new Date().toDateString()}</div>
+          <div>{time}</div>
+        </div>
+        <motion.div
+          className={styles.bottomLine}
+          initial={{ y: "100%" }}
+          animate={{ y: 0 }}
+          transition={{ default: { duration: 1.5 } }}
+        ></motion.div>
+      </div>
+      <div className={`${styles.enter} ${showEnter ? styles.enterShow : null}`}>
+        <Link to="/profile" className={styles.link}>
+          Enter
+          <KeyboardArrowDown />
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
